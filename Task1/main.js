3d46496b1a78619e86340a246c4a1d11665b4cd4
raudbtw <105887271+raudbtw@users.noreply.github.com>
2022-08-29 11:50:41 +0300

    Add files via upload

---
+++ Task1/main.js
@@ -0,0 +1,40 @@
+const imgs = [
+  "./img/1.png",
+  "./img/2.png",
+  "./img/3.png",
+  "./img/4.png",
+  "./img/5.png",
+  "./img/6.png",
+  "./img/7.png",
+  "./img/8.jpg",
+  "./img/9.png",
+];
+
+const nextButton = document.querySelector(".next");
+const prevButton = document.querySelector(".prev");
+const slideImg = document.querySelector(".slide-img");
+let currentImg = 0;
+
+nextButton.addEventListener("click", nextPic);
+prevButton.addEventListener("click", pervPic);
+
+slideImg.src = imgs[currentImg];
+prevButton.disabled = true;
+
+function nextPic() {
+  currentImg++;
+  if (currentImg === imgs.length - 1) {
+    nextButton.disabled = true;
+  }
+  prevButton.disabled = false;
+  slideImg.src = imgs[currentImg];
+}
+
+function pervPic() {
+  currentImg--;
+  slideImg.src = imgs[currentImg];
+  nextButton.disabled = false;
+  if (currentImg === 0) {
+    prevButton.disabled = true;
+  }
+}
